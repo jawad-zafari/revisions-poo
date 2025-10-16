ceee3cc3f1ffcfd8dc62807b70e126c33d99d0c9
jawad zafari <jawad.zafari@laplateforme.io>
2025-10-16 14:55:20 +0200

    echo les valeurs

--- job-01/index.php
+++ job-01/index.php
@@ -1,11 +1,13 @@
 <?php
-require 'Product.php'; 
+require_once('Product.php'); 
 
-$product = new Product('livre',1,'Un roman',10,['https://picsum.photos/200/300'],25, new DateTime(),new DateTime());
-
-var_dump($product);
-
-$product->setPrice(25);
-
-echo $product->getPrice();
+$product = new Product('livre',1,'Un roman',10,['https://www.photos.com/livre.png'],25, new DateTime(),new DateTime());
+print_r($product);
+echo "<hr>";
+echo $product->getName()."<br>";
+echo $product->getId()."<br>";
+echo $product->getQuantity()."<br>";
+echo $product->getPhotos()[0]."<br>";
+echo $product->getPrice()."<br>";
+echo $product->getDescription();
 ?>
