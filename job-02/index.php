6270c9326fe59d5f1605b5a4c9308fa50b3c5bb1
jawad zafari <jawad.zafari@laplateforme.io>
2025-10-16 15:12:37 +0200

    echo les object

--- job-02/index.php
+++ job-02/index.php
@@ -6,5 +6,12 @@ $category = new Category(1,'livre','Regroupement de livres',new DateTime(),new D
 
 $product = new Product(1,'livre',['https://www.photos.com/livre.png'],45,"Un roman",10,new DateTime(),new DateTime(),$category->getId() );
 
-
+var_dump($category);
+echo "<hr>";
+print_r($category);
+echo "<hr>";
+var_dump($product);
+echo "<hr>";
+print_r($product);
+echo "<hr>";
 ?>
