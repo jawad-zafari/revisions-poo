5ad9108c733b2b58416a70ca13f10c122a6c1248
jawad zafari <jawad.zafari@laplateforme.io>
2025-10-16 15:13:15 +0200

    echo les valeurs

--- job-02/index.php
+++ job-02/index.php
@@ -14,4 +14,14 @@ var_dump($product);
 echo "<hr>";
 print_r($product);
 echo "<hr>";
+echo "Category ID: " . $category->getId() . "<br>";
+echo "Category Name: " . $category->getName() . "<br>";
+echo "Category Description: " . $category->getDescription() . "<br>";
+echo "Name: " . $product->getName() . "<br>";
+echo "ID: " . $product->getId() . "<br>";
+echo "Quantity: " . $product->getQuantity() . "<br>";
+echo "Photo: " . $product->getPhotos()[0] . "<br>";
+echo "Price: " . $product->getPrice() . "<br>";
+echo "Description: " . $product->getDescription();
+
 ?>
