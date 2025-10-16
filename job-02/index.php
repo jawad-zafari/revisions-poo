3a6dd53ddb30eca910eb036719e5cb5bc244c58a
jawad zafari <jawad.zafari@laplateforme.io>
2025-10-16 15:11:59 +0200

    object product

--- job-02/index.php
+++ job-02/index.php
@@ -4,3 +4,7 @@ require 'Category.php';
 
 $category = new Category(1,'livre','Regroupement de livres',new DateTime(),new DateTime());
 
+$product = new Product(1,'livre',['https://www.photos.com/livre.png'],45,"Un roman",10,new DateTime(),new DateTime(),$category->getId() );
+
+
+?>
